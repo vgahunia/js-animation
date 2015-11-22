249fc21623d71fab5ac6d5784b0c9d02e39069af
Vivek Gahunia <vgahunia@gmail.com>
2015-11-22 14:18:15 -0500

    initial commit

---
+++ js/velocity_test.js
@@ -0,0 +1,20 @@
+
+$(document).ready(function() {
+	var $div = $('#box1');
+	
+	// appear
+	$('#button1').click(function() {
+		$div.velocity({width: "500px", opacity: 1},
+			{ duration: 1400, easing: "swing" });
+		$('#box1').css("background-color", "rgb(0, 0, 0");
+	})
+
+	//disappear
+	$('#button2').click(function() {
+		$div.velocity({width: "300px", opacity: 0});
+	})
+	
+
+	
+
+})
