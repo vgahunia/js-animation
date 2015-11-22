150171bada98e8a32787cdfdb5c9926d5ebbc891
Vivek Gahunia <vgahunia@gmail.com>
2015-11-22 15:59:50 -0500

    3rd commit

--- js/velocity_test.js
+++ js/velocity_test.js
@@ -4,16 +4,23 @@ $(document).ready(function() {
 	
 	// appear
 	$('#button1').click(function() {
-		$div.velocity({ top: 100}, 500)
-		$div.velocity({width: "500px", opacity: 1},
-			{ duration: 1400, easing: "swing" });
+		$div.velocity({ 
+			top: 50, 
+			paddingLeft: "*=4", 
+			paddingRight: "*=4" }, 
+			"ease-in-out");
+		$div.velocity({
+			width: "500px", 
+			opacity: 1},
+				{ duration: 1400, 
+					easing: [450, 15] });
 		$('#box1').css("background-color", "rgb(0, 0, 0");
 	})
 
-	//disappear
+	// disappear
 	$('#button2').click(function() {
-		$div.velocity({ top: 20}, 500)
-		$div.velocity({width: "300px", opacity: 0});
+		$div.velocity({ top: 20}, "ease-out")
+		$div.velocity({width: "300px", opacity: 0}, { loop: 2 }).velocity({ opacity: 0 }, 1500);
 	})
 	
 })
